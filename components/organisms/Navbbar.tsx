c6709eeb871e60b4e299dec3a293be9c18f4bac1
Oleg Chursin <olegchursin@gmail.com>
2022-04-10 16:39:54 -0400

    feat: add navbar, footer, and hero sections

---
+++ components/organisms/Navbbar.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Navbbar = () => {
+  return (
+    <div className="navbar bg-base-300">
+      <div className="navbar-start">
+        <a className="btn btn-ghost normal-case text-xl">
+          Ryan Cunningham Hair
+        </a>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Homepage</a>
+            </li>
+            <li>
+              <a>Portfolio</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,28 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import Footer from '../components/organisms/Footer';
+import Hero from '../components/organisms/Hero';
+import Navbbar from '../components/organisms/Navbbar';
 import Testimonials from '../components/organisms/Testimonials';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Ryan Cunningham Hair</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbbar />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+          <Hero />
+          <Testimonials />
+        </main>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <Testimonials />
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
